a01c54ad0af1fb668848e94bb32fd5d118dc3ee1
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2022-12-31 19:04:54 +0000

    fix exercice4

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ function isBlank(character){
      */
 
     if(character.toString().length == "") {
+
         return false;
+
     }
     return true;
 }
@@ -23,10 +25,22 @@ isBlank('');
 /* *************** Exercice 2 ******************* */
 
 function abbrevName(character) {
+
+    /**
+     * @description : cette fonction fais les abbréviations d'une chaine de caractère
+     * @param : character -> la chaine qu'on veut abbréger
+     * @param : abbrevTab -> représente la liste des mots contenus dans notre chaine
+     * @returns;
+     */
+
     let abbrevTab = character.toString().split(" ");
+
     if(abbrevTab.length > 1) {
+
         return `${abbrevTab[0]} ${abbrevTab[1].charAt(0)}.`;
+
     }
+
     return character;
 }
 
@@ -37,6 +51,14 @@ abbrevName("Robin Singh");
 /* *************** Exercice 3 ************** */
 
 function swapCase(character) {
+
+    /**
+     * @description : cette fonction permet de remplacer toutes les majuscules par des 
+     * miniscules et vice verca
+     * @param  : character -> chaine sur laquelle on travail
+     * @param : wordTable -> tableau qui contient les mots de notre chaine
+     * @param : neWordTableFormated -> tableau contenant les mots reformatter
+     */
 
     let wordTable = character.split(" ");
     let neWordTableFormated = [];
@@ -66,7 +88,33 @@ function swapCase(character) {
 
 function isOmnipresent(array, number) {
 
+    /**
+     * @description : cette fonction permet de vérifier si un chiffre est présente
+     * dans un tableau composé de sous tableau
+     * @param : array -> tableau de sous tableau
+     * @param : number -> nombre à vérifier dans le tableau
+     * @param : found -> le nombre de fois ou on trouve le nombre dans le tableau
+     */
+    
+    let found = 0;
+
+    for(let item of array) {
+        
+        for(let i of item) {
+            
+            if(i == number) {
+                found++;
+            }
+        }
+    }
+
+    if(found >= array.length) {
+        return true;
+    }
+    return false;
 
 }
+
+isOmnipresent([[1, 1], [1, 3], [5, 1], [6, 1]], 6);
 
 /* **************** End Exercice 4 **************** */
